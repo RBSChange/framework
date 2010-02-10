cfe98dddf8423d874ace9344d08a9f6efe8f2f7f
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-02-10 08:01:43 +0000

    [FIX] Correction patch framework 0305

--- patch/0305/install.php
+++ patch/0305/install.php
@@ -8,12 +8,11 @@ class framework_patch_0305 extends patch_BasePatch
 	 * you must split it into two different patches.
 	 * @return Boolean true if the patch modify code that is versionned.
 	 */
-		public function isCodePatch()
-		{
-			return true;
-		}
+	public function isCodePatch()
+	{
+		return true;
+	}
 	
-
 	/**
 	 * Entry point of the patch execution.
 	 */
@@ -22,69 +21,79 @@ class framework_patch_0305 extends patch_BasePatch
 		// Remove the following line and implement the patch here.
 		parent::execute();
 		
-		$modules = ModuleService::getInstance()->getModulesObj();		
+		$modules = ModuleService::getInstance()->getModulesObj();
 		foreach ($modules as $mod)
 		{
 			$stylePath = f_util_FileUtils::buildWebeditPath('modules', $mod->getName(), 'style');
-			if (is_dir($stylePath))
-			{
-				$files = scandir($stylePath);
-				foreach ($files as $fileName)
-				{
-					if (preg_match('/\.xml$/', $fileName))
-					{
-						$filePath = f_util_FileUtils::buildAbsolutePath($stylePath, $fileName);
-						if (is_writable($filePath))
-						{
-							$cssxml = new DOMDocument();
-							$cssxml->load($filePath);
-							if ($cssxml->documentElement)
-							{
-								$cssrule = $cssxml->getElementsByTagName('style');
-								if ($cssrule->length > 0)
-								{
+			$this->migratePath($mod, $stylePath);
 
-									
-									$css = f_web_CSSStylesheet::getInstanceFromFile($filePath);
-									$engines = $css->getAllEngine();
-									$cssContent = $css->getCSSForEngine('all.all');
-									foreach ($engines as $eng)
+			$stylePath = f_util_FileUtils::buildWebappPath('modules', $mod->getName(), 'style');
+			$this->migratePath($mod, $stylePath);
+		}
+	}
+	
+	/**
+	 * @param c_Module $mod
+	 * @param string $stylePath
+	 */
+	private function migratePath($mod, $stylePath)
+	{
+		if (is_dir($stylePath))
+		{
+			$files = scandir($stylePath);
+			foreach ($files as $fileName)
+			{
+				if (preg_match('/\.xml$/', $fileName))
+				{
+					$filePath = f_util_FileUtils::buildAbsolutePath($stylePath, $fileName);
+					if (is_writable($filePath))
+					{
+						$cssxml = new DOMDocument();
+						$cssxml->load($filePath);
+						if ($cssxml->documentElement)
+						{
+							$cssrule = $cssxml->getElementsByTagName('style');
+							if ($cssrule->length > 0)
+							{
+								
+								$css = f_web_CSSStylesheet::getInstanceFromFile($filePath);
+								$engines = $css->getAllEngine();
+								$cssContent = $css->getCSSForEngine('all.all');
+								foreach ($engines as $eng)
+								{
+									if ($eng === 'all.all')
 									{
-										if ($eng === 'all.all')
-										{
-											continue;
-										}
-										$cssContent .= "\n@import url(/modules/" . $mod->getName() . "/style/" . str_replace('.xml', '.' . $eng . '.css', $fileName) . ');';
-									
+										continue;
 									}
-									$cssPath = str_replace('.xml', '.css', $filePath);
-									echo "\nWriting CSS: $cssPath";
+									$cssContent .= "\n@import url(/modules/" . $mod->getName() . "/style/" . str_replace('.xml', '.' . $eng . '.css', $fileName) . ');';
+								
+								}
+								$cssPath = str_replace('.xml', '.css', $filePath);
+								echo "\nWriting CSS: $cssPath";
+								unlink($cssPath);
+								file_put_contents($cssPath, $cssContent);
+								
+								foreach ($engines as $eng)
+								{
+									if ($eng === 'all.all')
+									{
+										continue;
+									}
+									$cssPath = str_replace('.xml', '.' . $eng . '.css', $filePath);
+									echo "\nWriting Sub CSS: $cssPath";
+									$cssContent = $css->getCSSForEngine($eng);
 									unlink($cssPath);
 									file_put_contents($cssPath, $cssContent);
-									
-									foreach ($engines as $eng)
-									{
-										if ($eng === 'all.all')
-										{
-											continue;
-										}
-										$cssPath = str_replace('.xml', '.' . $eng . '.css', $filePath);
-										echo "\nWriting Sub CSS: $cssPath";
-										$cssContent = $css->getCSSForEngine($eng);
-										unlink($cssPath);
-										file_put_contents($cssPath, $cssContent);
-									}
-									
-									
 								}
-								echo "\nDelete: $filePath";
-								unlink($filePath);
+							
 							}
+							echo "\nDelete: $filePath";
+							unlink($filePath);
 						}
-						else
-						{
-							echo "\nLocked : $filePath";
-						}
+					}
+					else
+					{
+						echo "\nLocked : $filePath";
 					}
 				}
 			}
