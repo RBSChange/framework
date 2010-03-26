a7b2d4e532f3e2749ead439918ba728bc7565f1d
Franck STAUFFER <franck.stauffer@rbs.fr>
2010-03-26 12:18:19 +0000

    [FIX] Potential fatal error corrected

--- mail/NullMailService.class.php
+++ mail/NullMailService.class.php
@@ -38,15 +38,17 @@ class NullMailService extends MailService
 		if (!Framework::isDebugEnabled())
 		{
 			Framework::warn("You are currently using NullMailService - this should not be the case outside of DEBUG mode");
-			return;
 		}
-		Framework::debug(__METHOD__);
-		Framework::debug("Mail sender : " . $mailMessage->getSender());
-		Framework::debug("Mail receiver : " . $mailMessage->getReceiver());
-		Framework::debug("Mail reply to : " . $mailMessage->getReplyTo());
-		Framework::debug("Mail subject : " . $mailMessage->getSubject());
-		Framework::debug("Mail Html : " . $mailMessage->getHtmlContent());
-		Framework::debug("Mail Text : " . $mailMessage->getTextContent());
+		else
+		{
+			Framework::debug(__METHOD__);
+			Framework::debug("Mail sender : " . $mailMessage->getSender());
+			Framework::debug("Mail receiver : " . $mailMessage->getReceiver());
+			Framework::debug("Mail reply to : " . $mailMessage->getReplyTo());
+			Framework::debug("Mail subject : " . $mailMessage->getSubject());
+			Framework::debug("Mail Html : " . $mailMessage->getHtmlContent());
+			Framework::debug("Mail Text : " . $mailMessage->getTextContent());
+		}
 		return new MailerNull(array());
 	}
 }
