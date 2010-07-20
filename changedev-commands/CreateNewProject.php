761f0982b7eecfc28000af2f8be9c669da784390
Alexandre SIMON <alexandre.simon@rbs.fr>
2010-07-20 15:42:15 +0200

    [ENHANCE] launch changecron at create-new-project end

--- changedev-commands/CreateNewProject.php
+++ changedev-commands/CreateNewProject.php
@@ -32,6 +32,7 @@ class commands_CreateNewProject extends c_ChangescriptCommand
 		$parent->executeCommand("compileAll");
 		$parent->executeCommand("importInitData");
 		$parent->executeCommand("initPatchDb");
+		f_util_System::execHTTPScript('changecron.php', array(), true);
 		
 		$this->quitOk("Project is ready to use");
 	}
