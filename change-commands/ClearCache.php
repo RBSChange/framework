65e29daa23396f82f5379d403230f6081fc7aac3
Alexandre SIMON <alexandre.simon@rbs.fr>
2010-08-10 18:22:09 +0200

    clear-cache now uses FileUtils

--- change-commands/ClearCache.php
+++ change-commands/ClearCache.php
@@ -31,58 +31,24 @@ class commands_ClearCache extends commands_AbstractChangeCommand
 	{
 		$this->message("== Clear cache ==");
 
-		// TODO: recode... to old fashionned
-
 		$profile = $this->getProfile();
-
 		if ( ! defined('WEBEDIT_HOME') )
 		{
 			define('WEBEDIT_HOME', realpath('.'));
 		}
 
 		$cacheProfileDirectory = WEBEDIT_HOME . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR . $profile;
-		$autoloadCache = $cacheProfileDirectory.DIRECTORY_SEPARATOR."autoload";
 		$wwwCache = WEBEDIT_HOME . DIRECTORY_SEPARATOR .  'cache' . DIRECTORY_SEPARATOR . 'www';
-
 		// Test if the cache directory of selected profile exist
 		if ( is_dir( $cacheProfileDirectory ) )
 		{
-			if (is_dir($autoloadCache))
-			{
-				$this->deleteRecursively($autoloadCache, true);
-			}
-			$this->deleteRecursively($cacheProfileDirectory);
+			f_util_FileUtils::rmdir($cacheProfileDirectory, true);
 			if (is_dir($wwwCache))
 			{
-				$this->deleteRecursively($wwwCache);
+				f_util_FileUtils::rmdir($wwwCache, true);
 			}
-
 			return $this->quitOk("Cache cleared");
 		}
 		return $this->quitError("The profile $profile does not exists");
-	}
-
-	private function deleteRecursively( $directory, $includeDir = false)
-	{
-		$listOfFiles = scandir( $directory );
-		foreach ($listOfFiles as $file)
-		{
-			$absFile = $directory . DIRECTORY_SEPARATOR . $file;
-			if ( is_dir($absFile) && !is_link($absFile) )
-			{
-				if ( $file !== '.' && $file !== '..')
-				{
-					$this->deleteRecursively($absFile, true);
-				}
-			}
-			else
-			{
-				unlink( $absFile );
-			}
-		}
-		if ($includeDir)
-		{
-			rmdir($directory);
-		}
 	}
 }
