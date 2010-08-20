a17c66747a86c30803378e9982096a105b0376bd
Kévin O'NEILL <kevin.oneill@rbs.fr>
2010-08-20 14:51:56 +0200

    [FIX] node dispatch

--- cache/DataCacheMemcacheService.php
+++ cache/DataCacheMemcacheService.php
@@ -50,9 +50,8 @@ class f_DataCacheMemcacheService extends f_DataCacheService
 	/**
 	 * @param f_DataCacheItem $item
 	 * @param String $subCache
-	 * @param Boolean $dispatch (optional)
 	 */
-	public final function clearSubCache($item, $subCache, $dispatch = true)
+	public final function clearSubCache($item, $subCache)
 	{
 		$this->registerShutdown();
 		
@@ -71,8 +70,6 @@ class f_DataCacheMemcacheService extends f_DataCacheService
 		{
 			$this->idToClear[$item->getNamespace()][$item->getKeyParameters()] = $subCache;
 		}
-
-		$this->dispatch = $dispatch || $this->dispatch;
 	}
 	
 	public function clearCommand()
