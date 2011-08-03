4f948a36635314dd0a02f9ff62d93f75c05f8235
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-08-03 14:12:52 +0200

    [UPDATE] Utilisation de de Mail_Mime en temps que package PEAR

---
+++ libs/mvc/ActionStack.class.php
@@ -0,0 +1,108 @@
+<?php
+class change_ActionStack
+{
+	private $stack = array();
+
+	/**
+	 * @param string $moduleName
+	 * @param string $actionName
+	 * @param change_ActionStackEntry $actionInstance
+	 */
+	public function addEntry ($moduleName, $actionName, $actionInstance)
+	{
+
+		$actionEntry = new change_ActionStackEntry($moduleName, $actionName, $actionInstance);
+		$this->stack[] = $actionEntry;
+		return $actionEntry;
+	}
+
+	/**
+	 * @param integer $index
+	 * @return change_ActionStackEntry
+	 */
+	public function getEntry($index)
+	{
+		$retval = null;
+		if ($index > -1 && $index < count($this->stack))
+		{
+			$retval = $this->stack[$index];
+		}
+		return $retval;
+	}
+
+	/**
+	 * @return change_ActionStackEntry
+	 */
+	public function getFirstEntry ()
+	{
+		$count  = count($this->stack);
+		$retval = null;
+		if ($count > 0)
+		{
+			$retval = $this->stack[0];
+		}
+		return $retval;
+	}
+
+	/**
+	 * @return change_ActionStackEntry
+	 */
+	public function getLastEntry ()
+	{
+		$count  = count($this->stack);
+		$retval = null;
+		if ($count > 0)
+		{
+			$retval = $this->stack[$count - 1];
+		}
+		return $retval;
+	}
+
+	/**
+	 * @return integer
+	 */
+	public function getSize ()
+	{
+		return count($this->stack);
+	}
+}
+
+class change_ActionStackEntry
+{
+	private $actionInstance = null;
+	private $actionName     = null;
+	private $microtime      = null;
+	private $moduleName     = null;
+	private $presentation   = null;
+	
+	public function __construct ($moduleName, $actionName, $actionInstance)
+	{
+		
+		$this->actionName     = $actionName;
+		$this->actionInstance = $actionInstance;
+		$this->microtime      = microtime();
+		$this->moduleName     = $moduleName;
+		
+	}
+	
+	public function getActionName ()
+	{	
+		return $this->actionName;
+	}
+	
+	public function getActionInstance ()
+	{
+		return $this->actionInstance;
+	}
+	
+
+	public function getMicrotime ()
+	{	
+		return $this->microtime;
+	}
+	
+	public function getModuleName ()
+	{
+		return $this->moduleName;
+	}
+}
---
+++ libs/mvc/JSONAction.class.php
@@ -0,0 +1,92 @@
+<?php
+abstract class change_JSONAction extends change_Action
+{
+	
+	/**
+	 * @param change_Request $request
+	 * @param Exception $e
+	 * @param Boolean $popupAlert
+	 * @return String
+	 */
+	protected function onBackOfficeException($request, $e, $popupAlert)
+	{
+		return $this->sendJSONException($e, $popupAlert);
+	}
+	
+	/**
+	 * @param array $content
+	 * @return null
+	 */
+	protected function sendJSON($content)
+	{
+		if (!headers_sent())
+		{
+			change_Controller::setNoCache();
+			header('Content-Type: application/json; charset=utf-8');
+		}
+		$message = array('status' => 'OK', 
+						'module' => $this->getModuleName(null),
+						'action' => $this->getActionName(null),
+						'contents' => $content);
+		echo JsonService::getInstance()->encode($message);
+		return change_View::NONE;			
+	}
+	
+	/**
+	 * @param String $message
+	 * @return unknown
+	 */
+	protected function sendJSONError($message, $popupAlert = true)
+	{
+		if (!headers_sent())
+		{
+			change_Controller::setNoCache();
+			header('Content-Type: application/json; charset=utf-8');
+		}
+		$message = array('status' => 'ERROR', 
+						'module' => $this->getModuleName(null),
+						'action' => $this->getActionName(null),
+						'contents' => array('popupAlert' => $popupAlert,
+											'errorMessage' => $message));
+		echo JsonService::getInstance()->encode($message);
+		return change_View::NONE;			
+	}
+	
+	/**
+	 * @param Exception $e
+	 * @return null
+	 */
+	protected function sendJSONException($e, $popupAlert = true)
+	{
+		if (!headers_sent())
+		{
+			change_Controller::setNoCache();
+			header('Content-Type: application/json; charset=utf-8');
+		}
+		$message = array('status' => 'ERROR', 
+						'module' => $this->getModuleName(null),
+						'action' => $this->getActionName(null),
+						'contents' => array('popupAlert' => $popupAlert,
+											'errorMessage' => $e->getMessage(), 
+											'errorStackTrace' => $e->getTraceAsString()));
+		
+		if ($e instanceof BaseException ) 
+		{
+			$message['contents']['errorLocalKey'] = $e->getKey();
+			$message['contents']['errorOriginalMessage'] = $message['contents']['errorMessage'];
+			$message['contents']['errorMessage'] = $e->getLocaleMessage();
+		}
+		
+		echo JsonService::getInstance()->encode($message);
+		return change_View::NONE;			
+	}
+	
+	/**
+	 * @see f_action_BaseAction::getActionName()
+	 * @return String
+	 */
+	protected function getActionName()
+	{
+		return str_replace('JSON', '', parent::getActionName());
+	}
+}
