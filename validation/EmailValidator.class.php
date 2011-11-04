a8efbac44031c97c108e3bf8aa870d5e6d5fc2d8
Gael PORT <gael.port@rbs.fr>
2011-11-04 17:37:51 +0100

    [FIX #46269] Validation d'adresse e-mail erronée

--- validation/EmailValidator.class.php
+++ validation/EmailValidator.class.php
@@ -4,7 +4,10 @@
  */
 class validation_EmailValidator extends validation_ValidatorImpl implements validation_Validator
 {
-	const EMAIL_REGEXP = '/^[a-z0-9][a-z0-9_.-]*@[a-z0-9][a-z0-9.-]*\.[a-z]{2,}$/i';
+	/**
+	 * FIX #46269 - source http://atranchant.developpez.com/code/validation/
+	 */
+	const EMAIL_REGEXP = '/^[^\t,@]+@[^\s\t,@]{1,63}\.[a-z0-9]{2,10}$/i';
 
 
 	public function __construct()
@@ -26,13 +29,29 @@ class validation_EmailValidator extends validation_ValidatorImpl implements vali
 		if ($this->getParameter() == true)
 		{
 			$value = $field->getValue();
-			if ($value !== null && (!is_string($value) || ($value !== "" && !preg_match(self::EMAIL_REGEXP, $value))))
+			if ($value !== null && $value !== '' && !self::isEmail($value))
 			{
 				$this->reject($field->getName(), $errors);
 			}
 		}
 	}
 
+	/**
+	 * @param string $email
+	 * @return boolean
+	 */
+	public static function isEmail($email)
+	{
+		if (!is_string($email) || $email === '' || strlen($email) > 255)
+		{
+			return false;
+		}
+		elseif (function_exists('filter_var'))
+		{
+			return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+		}
+		return preg_match(self::EMAIL_REGEXP, $email);
+	}
 
 	/**
 	 * Sets the value of the unique validator's parameter.
