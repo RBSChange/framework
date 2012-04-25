9145df90a8c62a1b15556cc838d7129368e6f7fc
Gaël PORT <gael.port@rbs.fr>
2012-04-25 09:30:04 +0200

    [FIX] generic_FolderService class name case.

--- patch/0361/install.php
+++ patch/0361/install.php
@@ -29,61 +29,60 @@ class framework_patch_0361 extends patch_BasePatch
 			}
 		}
 		
-		foreach (generic_folderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_order/waitingresponseorderfolder'))->find() as $folder)
+		foreach (generic_FolderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_order/waitingresponseorderfolder'))->find() as $folder)
 		{
 			/* @var $folder generic_persistentdocument_folder */
 			$folder->setLabel('m.order.document.waitingresponseorderfolder.document-name');
 			$folder->save();
 		}
 		
-		foreach (generic_folderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_catalog/noshelfproductfolder'))->find() as $folder)
+		foreach (generic_FolderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_catalog/noshelfproductfolder'))->find() as $folder)
 		{
 			/* @var $folder generic_persistentdocument_folder */
 			$folder->setLabel('m.catalog.document.noshelfproductfolder.document-name');
 			$folder->save();
 		}
 		
-		foreach (generic_folderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_catalog/shopfolder'))->find() as $folder)
+		foreach (generic_FolderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_catalog/shopfolder'))->find() as $folder)
 		{
 			/* @var $folder generic_persistentdocument_folder */
 			$folder->setLabel('m.catalog.document.shopfolder.document-name');
 			$folder->save();
 		}
 		
-		foreach (generic_folderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_customer/tarifcustomergroupfolder'))->find() as $folder)
+		foreach (generic_FolderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_customer/tarifcustomergroupfolder'))->find() as $folder)
 		{
 			/* @var $folder generic_persistentdocument_folder */
 			$folder->setLabel('m.customer.document.tarifcustomergroupfolder.document-name');
 			$folder->save();
 		}
 		
-		foreach (generic_folderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_customer/voucherfolder'))->find() as $folder)
+		foreach (generic_FolderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_customer/voucherfolder'))->find() as $folder)
 		{
 			/* @var $folder generic_persistentdocument_folder */
 			$folder->setLabel('m.customer.document.voucherfolder.document-name');
 			$folder->save();
 		}
 		
-		foreach (generic_folderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_form/recipientGroupFolder'))->find() as $folder)
+		foreach (generic_FolderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_form/recipientGroupFolder'))->find() as $folder)
 		{
 			/* @var $folder generic_persistentdocument_folder */
 			$folder->setLabel('m.form.document.recipientgroupfolder.document-name');
 			$folder->save();
 		}
 		
-		foreach (generic_folderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_productreturns/paymentfolder'))->find() as $folder)
+		foreach (generic_FolderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_productreturns/paymentfolder'))->find() as $folder)
 		{
 			/* @var $folder generic_persistentdocument_folder */
 			$folder->setLabel('m.productreturns.document.paymentfolder.label-content');
 			$folder->save();
 		}
 		
-		foreach (generic_folderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_productreturns/reasonfolder'))->find() as $folder)
+		foreach (generic_FolderService::getInstance()->createQuery()->add(Restrictions::eq('model', 'modules_productreturns/reasonfolder'))->find() as $folder)
 		{
 			/* @var $folder generic_persistentdocument_folder */
 			$folder->setLabel('m.productreturns.document.reasonfolder.label-content');
 			$folder->save();
 		}
-	
 	}
 }
