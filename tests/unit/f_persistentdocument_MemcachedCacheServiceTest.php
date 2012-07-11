899f100df30dea46ca99f7872b7b534b121b9af4
Loïc Couturier <loic.couturier@rbs.fr>
2012-07-11 11:56:37 +0200

    [NEW] Add untestable class

---
+++ tests/unit/f_persistentdocument_MemcachedCacheServiceTest.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * f_persistentdocument_MemcachedCacheService test case.
+ */
+class f_persistentdocument_MemcachedCacheServiceTest extends testing_UnitTestBase
+{
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp()
+	{
+		parent::setUp($this->getName());
+	}
+
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown()
+	{
+		parent::tearDown();
+	}
+
+	/**
+	 * Constructs the test case.
+	 */
+	public function __construct()
+	{
+	}
+
+	/**
+	 * Tests f_persistentdocument_MemcachedCacheService->__destruct()
+	 * @see f_persistentdocument_MemcachedCacheService::__destruct
+	 */
+	public function test__destruct()
+	{
+		$this->markTestSkipped();
+	}
+	
+	/**
+	 * Tests f_persistentdocument_MemcachedCacheService->getInstance()
+	 * @see f_persistentdocument_MemcachedCacheService::getInstance
+	 */
+	public function testGetInstance()
+	{
+		$this->markTestSkipped();
+	}
+	
+	/**
+	 * Tests f_persistentdocument_MemcachedCacheService->get()
+	 * @see f_persistentdocument_MemcachedCacheService::get
+	 */
+	public function testGet()
+	{
+		$this->markTestSkipped();
+	}
+	
+	/**
+	 * Tests f_persistentdocument_MemcachedCacheService->getMultiple()
+	 * @see f_persistentdocument_MemcachedCacheService::getMultiple
+	 */
+	public function testGetMultiple()
+	{
+		$this->markTestSkipped();
+	}
+	
+	/**
+	 * Tests f_persistentdocument_MemcachedCacheService->set()
+	 * @see f_persistentdocument_MemcachedCacheService::set
+	 */
+	public function testSet()
+	{
+		$this->markTestSkipped();
+	}
+	
+	/**
+	 * Tests f_persistentdocument_MemcachedCacheService->update()
+	 * @see f_persistentdocument_MemcachedCacheService::update
+	 */
+	public function testUpdate()
+	{
+		$this->markTestSkipped();
+	}
+	
+	/**
+	 * Tests f_persistentdocument_MemcachedCacheService->clear()
+	 * @see f_persistentdocument_MemcachedCacheService::clear
+	 */
+	public function testClear()
+	{
+		$this->markTestSkipped();
+	}
+	
+	/**
+	 * Tests f_persistentdocument_MemcachedCacheService->beginTransaction()
+	 * @see f_persistentdocument_MemcachedCacheService::beginTransaction
+	 */
+	public function testBeginTransaction()
+	{
+		$this->markTestSkipped();
+	}
+	
+	/**
+	 * Tests f_persistentdocument_MemcachedCacheService->commit()
+	 * @see f_persistentdocument_MemcachedCacheService::commit
+	 */
+	public function testCommit()
+	{
+		$this->markTestSkipped();
+	}
+	
+	/**
+	 * Tests f_persistentdocument_MemcachedCacheService->rollBack()
+	 * @see f_persistentdocument_MemcachedCacheService::rollBack
+	 */
+	public function testRollBack()
+	{
+		$this->markTestSkipped();
+	}
+	
+}
