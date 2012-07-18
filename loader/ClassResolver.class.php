74fabd2e2c9d80ceb0dac5398fc14b9f325e2457
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-07-18 15:29:41 +0200

    [FIX #63005] Possibilité de visualiser du code php après un update-autoload

--- loader/ClassResolver.class.php
+++ loader/ClassResolver.class.php
@@ -217,6 +217,10 @@ class ClassResolver implements ResourceResolver
 	 */
 	public function initialize()
 	{
+		$htAccess = f_util_FileUtils::buildWebeditPath("framework", "builder", "home", "bin", ".htaccess");
+		$to = $this->cacheDir . DIRECTORY_SEPARATOR . ".htaccess";		
+		f_util_FileUtils::cp($htAccess, $to, f_util_FileUtils::OVERRIDE);
+		
 		$ini = $this->getPathsToAnalyse();
 
 		// let's do our fancy work
