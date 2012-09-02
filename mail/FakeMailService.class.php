c4b0d003354bb43f3cc22ae6eb5cac291e123f72
Franck STAUFFER <franck.stauffer@rbs.fr>
2012-09-02 16:05:21 +0200

    [REFACTOR] FakeMailService refactored to avoid code duplicaiton

--- mail/FakeMailService.class.php
+++ mail/FakeMailService.class.php
@@ -20,27 +20,16 @@ class FakeMailService extends MailService
 	        throw new Exception('The constant FAKE_EMAIL is not defined');
 	    }
 	    
-		$mailer = $this->getMailer();
-		// Pass the mailMessage to the mailer
-		$mailer->setSender($mailMessage->getSender());
-		$receiver = $mailMessage->getReceiver();
+		$mailer = parent::buildMailer($mailMessage);
+		$receiver = $mailer->getReceiver();
 		$mailer->setReceiver(FAKE_EMAIL);
 		$mailer->setBcc('');
 		$mailer->setCc('');
-		$mailer->setEncoding($mailMessage->getEncoding());
-		$mailer->setHtmlAndTextBody($mailMessage->getHtmlContent(), $mailMessage->getTextContent());
-		$mailer->setReplyTo($mailMessage->getReplyTo());
 		$mailer->setSubject("[Fake] ".$mailMessage->getSubject() . " [$receiver]");
-		
 		if ($mailMessage->hasNotificationTo())
 		{
 			$mailer->setHeader('Disposition-Notification-To', FAKE_EMAIL);
 		}
-		
-		foreach ($mailMessage->getAttachment() as $attachement)
-		{
-			$mailer->addAttachment($attachement);
-		}	
 		return $mailer;
 	}
 }
