19bb16f56de4f6f5075d20349e1e5b7498b577fd
Loïc Couturier <loic.couturier@rbs.fr>
2013-01-11 16:35:14 +0100

    [UPDATE] Active the check of autologin all time

--- libs/mvc/ChangeController.class.php
+++ libs/mvc/ChangeController.class.php
@@ -1,6 +1,8 @@
 <?php
 class controller_ChangeController extends HttpController
 {
+	const AUTO_LOGIN_COOKIE = 'autologin';
+	
 	/**
 	 * @return Request
 	 */
@@ -30,6 +32,30 @@ class controller_ChangeController extends HttpController
 
 	public function dispatch()
 	{
+		// Handle auto-login.
+		$us = users_UserService::getInstance();
+		if (is_null($us->getCurrentFrontEndUser()) && isset($_COOKIE[self::AUTO_LOGIN_COOKIE]))
+		{
+			$autoLoginInfos = $_COOKIE[self::AUTO_LOGIN_COOKIE];
+			$login = $autoLoginInfos['login'];
+			$passwd = $autoLoginInfos['passwd'];
+				
+			$website = website_WebsiteModuleService::getInstance()->getCurrentWebsite();
+			$user = $us->getFrontendUserByLogin($login, $website->getId());
+			if ($user !== null && $user->isPublished() && sha1($user->getPasswordmd5()) == $passwd)
+			{
+				if (Framework::isDebugEnabled())
+				{
+					Framework::debug(__METHOD__ . ' auto-login with ' . $login);
+				}
+				$us->authenticateFrontEndUser($user);
+			}
+			else if (Framework::isDebugEnabled())
+			{
+				Framework::debug(__METHOD__ . ' auto-login skipped because the user with login ' . $login . ' doesn\'t exist, is not published or the password is wrong.');
+			}
+		}
+		
 		$this->setLangFromRequest();
 		$this->setRequestContextMode();
 		parent::dispatch();
