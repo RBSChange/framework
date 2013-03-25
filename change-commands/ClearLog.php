c22ed6df3ad23453df622bc326847b9580c790f9
Gaël PORT <gael.port@rbs.fr>
2013-03-25 15:39:29 +0100

    [FIX #76441] clear-log ne vide pas les logs de l'indexer => new parameter to select the subfolder.

--- change-commands/ClearLog.php
+++ change-commands/ClearLog.php
@@ -2,62 +2,85 @@
 class commands_ClearLog extends commands_AbstractChangeCommand
 {
 	/**
-	 * @return String
+	 * @return string
 	 */
-	function getUsage()
+	public function getUsage()
 	{
-		return "";
+		return "[<loggerName>]";
 	}
 	
-	function getAlias()
+	/**
+	 * @return string
+	 */
+	public function getAlias()
 	{
 		return "cl";
 	}
-
+	
 	/**
-	 * @return String
+	 * @return string
 	 */
-	function getDescription()
+	public function getDescription()
 	{
-		return "clear log files";
+		return "Clear log files for the sepcified logger. If no logger specified, the default one (profile) is cleared.";
 	}
-
+	
 	/**
-	 * @param String[] $params
-	 * @param array<String, String> $options where the option array key is the option name, the potential option value or true
+	 * @param Integer $completeParamCount the parameters that are already complete in the command line
+	 * @param string[] $params
+	 * @param array<string, string> $options where the option array key is the option name, the potential option value or true
+	 * @return string[] or null
+	 */
+	public function getParameters($completeParamCount, $params, $options, $current)
+	{
+		if ($completeParamCount == 0)
+		{
+			$components = array();
+			foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(WEBEDIT_HOME . DIRECTORY_SEPARATOR . 'log', RecursiveDirectoryIterator::KEY_AS_PATHNAME), RecursiveIteratorIterator::SELF_FIRST) as $file => $info)
+			{
+				/* @var $info SplFileInfo */
+				if ($info->isDir() && $info->getFilename() != '.' && $info->getFilename() != '..')
+				{
+					$components[] = $info->getFilename();
+				}
+			}
+			return $components;
+		}
+		return null;
+	}
+	
+	/**
+	 * @param string[] $params
+	 * @param array<string, string> $options where the option array key is the option name, the potential option value or true
 	 * @see c_ChangescriptCommand::parseArgs($args)
 	 */
-	function _execute($params, $options)
+	public function _execute($params, $options)
 	{
 		$this->message("== Clear log ==");
-
+		
+		$logger = isset($params[0]) ? $params[0] : $this->getProfile();
 		$profile = $this->getProfile();
-
-		if ( ! defined('WEBEDIT_HOME') )
+		if (!defined('WEBEDIT_HOME'))
 		{
 			define('WEBEDIT_HOME', realpath('.'));
 		}
-
-		$logProfileDirectory = WEBEDIT_HOME . DIRECTORY_SEPARATOR . 'log' . DIRECTORY_SEPARATOR . $profile;
-
-		// Test if the log directory of selected profile exist
-		if ( is_dir( $logProfileDirectory ) )
+		
+		// Test if the log directory of selected profile exist.
+		$logProfileDirectory = WEBEDIT_HOME . DIRECTORY_SEPARATOR . 'log' . DIRECTORY_SEPARATOR . $logger;
+		if (is_dir($logProfileDirectory))
 		{
-			$listOfFiles = scandir( $logProfileDirectory );
-
-			foreach ($listOfFiles as $file)
+			foreach (scandir($logProfileDirectory) as $file)
 			{
-				if ( ! is_dir($logProfileDirectory . DIRECTORY_SEPARATOR . $file) )
+				if (!is_dir($logProfileDirectory . DIRECTORY_SEPARATOR . $file))
 				{
-					unlink( $logProfileDirectory . DIRECTORY_SEPARATOR . $file );
+					unlink($logProfileDirectory . DIRECTORY_SEPARATOR . $file);
 				}
 			}
 		}
 		else
 		{
-			return $this->quitError('The profile ' . $profile . ' doest not exist.');
+			return $this->quitError('No folder for logger "' . $logger . '".');
 		}
-
-		return $this->quitOk("Log files cleared");
+		return $this->quitOk('Log files cleared for logger "' . $logger . '".');
 	}
 }
